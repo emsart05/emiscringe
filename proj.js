e651e0fb513e811be212b02c55c7a6f1b609516a
emsart05 <98356731+emsart05@users.noreply.github.com>
2022-08-18 17:31:59 -0400

    Add files via upload

---
+++ proj.js
@@ -0,0 +1,68 @@
+// Elements
+let projBlock = $(".proj-block")
+let projImg = $(".proj-ss")
+let navBar = $("#projects-nav")
+let navItem = $(".proj-nav-item")
+let body = $("#projects-body")
+let projTitle = $(".proj-title")
+let cap = $(".cap")
+
+// When page loads
+$(document).ready(function(){
+    // Window width less than 500 pixels
+    if($(window).width() < 500) {
+        // Project blocks
+        projBlock.css("grid-template-rows", "repeat(3 33%)")
+        projBlock.css("left", "8px")
+
+        // Screenshots
+        projImg.css("width", "250px")
+        projImg.css("margin-top", "5px")
+
+        // Nav bar
+        navBar.css("flex-wrap", "wrap")
+
+        // Nav items
+        navItem.css("font-size", "10px")
+        navItem.css("padding", "8px")
+
+        // Body
+        body.css("top", "45px")
+
+        // Title
+        projTitle.css("font-size", "20px")
+
+        // Caption
+        cap.css("font-size", "14px")
+        cap.css("padding-top", "15px")
+        cap.css("padding-bottom", "15px")
+    }
+
+    // Window width between 500 and 700
+    else if($(window).width() < 700 && $(window).width() > 500) {
+        // Nav items
+        navItem.css("font-size", "10px")
+        navItem.css("padding", "8px")
+
+        // Project blocks
+        projBlock.css("top", "15px")
+
+        // Screenshots
+        projImg.css("margin-top", "10px")
+
+        // Title
+        projTitle.css("font-size", "20px")
+
+        // Caption
+        cap.css("font-size", "15px")
+    }
+
+    // Window width between 700 and 1050
+    else if($(window).width() < 1050 && $(window).width() > 700) {
+        // Project blocks
+        projBlock.css("grid-template-columns", "repeat(3 33%)")
+        navItem.css("padding", "10px")
+        navItem.css("padding-top", "15px")
+        navItem.css("font-size", "15px")
+    }
+});
