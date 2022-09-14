d357b10057aa5285b95eaeb215466940f3d1643f
emsart05 <98356731+emsart05@users.noreply.github.com>
2022-09-13 20:08:33 -0400

    Add files via upload

---
+++ jquery/indexQuery.js
@@ -0,0 +1,16 @@
+
+// Navbar growth animation
+$("#webArtLink").on("mouseenter", function() {
+    $("#webArtLink").animate({fontSize: 28}, "fast")
+})
+$("#webArtLink").on("mouseleave", function() {
+    $("#webArtLink").animate({fontSize: 25}, "fast")
+})
+
+$("#projLink").on("mouseenter", function() {
+    $("#projLink").animate({fontSize: 28}, "fast")
+})
+$("#projLink").on("mouseleave", function() {
+    $("#projLink").animate({fontSize: 25}, "fast")
+})
+
