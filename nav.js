fcd44007a6abdcff36138a9dad1839e6c39ecf3c
emsart05 <98356731+emsart05@users.noreply.github.com>
2022-09-13 21:45:14 -0400

    Add files via upload

--- nav.js
+++ nav.js
@@ -3,11 +3,13 @@ const webArtLink = document.createElement("a")
 const projects = document.createElement("a")
 
 // WEB ART
+webArtLink.id = "webArtLink"
 webArtLink.className = "navLinks"
 webArtLink.innerHTML = "Web 'Art'"
 webArtLink.href = "https://www.emiscringe.ml/webArt"
 
 // PROJECTS
+projects.id = "projLink"
 projects.className = "navLinks"
 projects.innerHTML = "Projects"
 projects.href = "https://www.emiscringe.ml/projects"
