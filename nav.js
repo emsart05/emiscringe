5a81c0126da2b33a5172bb43cfa4834a995dc8c4
emsart05 <98356731+emsart05@users.noreply.github.com>
2023-01-19 20:58:27 -0500

    add blog link

--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 const bar = document.getElementById("navBar")
 const webArtLink = document.createElement("a")
 const projects = document.createElement("a")
+const blog = document.createElement("a")
 
 // WEB ART
 webArtLink.id = "webArtLink"
@@ -14,5 +15,12 @@ projects.className = "navLinks"
 projects.innerHTML = "Projects"
 projects.href = "https://www.emiscringe.ml/projects"
 
+// BLOGS
+blog.id = "blogLink"
+blog.className = "navLinks"
+blog.innerHTML= "Blog"
+blog.href = "https://www.emiscringe.ml/blog"
+
 bar.appendChild(webArtLink)
 bar.appendChild(projects)
+bar.appendChild(blog)
